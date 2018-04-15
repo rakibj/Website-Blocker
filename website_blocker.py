d6d27842b3419dcb8372206e25188088be541140
rakibj <rakibjahan@iut-dhaka.edu>
2018-04-15 21:50:27 +0600

    4.Removing websites from hostfile in time

--- website_blocker.py
+++ website_blocker.py
@@ -8,8 +8,9 @@ website_list = ["www.amazon.com", "daraz.com.bd"]
 
 while True:
     print(1)
+    newfile = ""
     if dt(dt.now().year, dt.now().month, dt.now().day, 8) < dt.now() < dt(dt.now().year, dt.now().month, dt.now().day,
-                                                                          20):
+                                                                          10):
         print("Working hours...")
         with open(hosts_temp, 'r+') as file:
             content = file.read()
@@ -17,5 +18,18 @@ while True:
                 if website not in content:
                     file.write("\n" + redirect + " " + website)
     else:
+        c = 0
         print("Free hours...")
+        with open(hosts_temp, "r") as file:
+            content = file.readlines()
+        with open(hosts_temp, "w") as file:
+            for line in content:
+                for website in website_list:
+                    if website in line:
+                        c = c + 1
+                if c == 0:
+                    file.write(line)
+                else:
+                    c = 0
+
     time.sleep(5)
